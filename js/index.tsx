268df4cccd66dc7f48a65e0f72b12fe5ad00682c
Michael Jungo <michaeljungo92@gmail.com>
2019-12-20 11:55:07 +0100

    Customise scrollbar appearance

--- js/index.tsx
+++ js/index.tsx
@@ -4,6 +4,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 
+const scrollbar = {
+  colour: {
+    fg: "rgba(0, 0, 0, 0.2)",
+    bg : "transparent",
+    hover : "rgba(0, 0, 0, 0.3)",
+    active : "rgba(0, 0, 0, 0.5)",
+  },
+  size : "0.5rem",
+};
+
 injectGlobal`${resetCss}
 html, body {
   height: 100%;
@@ -12,6 +22,30 @@ html, body {
 #root {
   height: 100%;
   overflow: hidden;
-}`
+}
+
+
+* {
+  scrollbar-color: ${scrollbar.colour.fg} ${scrollbar.colour.bg};
+  scrollbar-width: thin;
+}
+*::-webkit-scrollbar {
+  height: ${scrollbar.size};
+  width: ${scrollbar.size};
+}
+*::-webkit-scrollbar-track {
+  background-color: ${scrollbar.colour.bg};
+}
+*::-webkit-scrollbar-thumb {
+  background-color: ${scrollbar.colour.fg};
+  border-radius: 4px;
+}
+*::-webkit-scrollbar-thumb:hover {
+  background-color: ${scrollbar.colour.hover};
+}
+*::-webkit-scrollbar-thumb:active {
+  background-color: ${scrollbar.colour.active};
+}
+`
 
 ReactDOM.render(<App />, document.getElementById("root"));
