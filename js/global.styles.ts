82732a32fb98ae688083f6623a14260d7fc41f84
Michael Jungo <michaeljungo92@gmail.com>
2019-12-21 19:11:46 +0100

    Break the global styles into categories

---
+++ js/global.styles.ts
@@ -0,0 +1,78 @@
+export const fixHeight = `
+html, body {
+  height: 100%;
+}
+
+#root {
+  height: 100%;
+  overflow: hidden;
+}
+`;
+
+const scrollbarConfig = {
+  colour: {
+    fg: "rgba(0, 0, 0, 0.2)",
+    bg: "transparent",
+    hover: "rgba(0, 0, 0, 0.3)",
+    active: "rgba(0, 0, 0, 0.5)"
+  },
+  size: "0.5rem"
+};
+
+export const scrollbar = `
+* {
+  scrollbar-color: ${scrollbarConfig.colour.fg} ${scrollbarConfig.colour.bg};
+  scrollbar-width: thin;
+}
+*::-webkit-scrollbar {
+  height: ${scrollbarConfig.size};
+  width: ${scrollbarConfig.size};
+}
+*::-webkit-scrollbar-track {
+  background-color: ${scrollbarConfig.colour.bg};
+}
+*::-webkit-scrollbar-thumb {
+  background-color: ${scrollbarConfig.colour.fg};
+  border-radius: 4px;
+}
+*::-webkit-scrollbar-thumb:hover {
+  background-color: ${scrollbarConfig.colour.hover};
+}
+*::-webkit-scrollbar-thumb:active {
+  background-color: ${scrollbarConfig.colour.active};
+}
+`;
+
+// Apex charts are not that well integrated into React, therefore the styling
+// is done with global CSS styles and often requires !important to overwrite
+// their styles
+export const apexcharts = `
+.apexcharts-tooltip {
+  line-height: 0.5;
+  padding: 0.3rem;
+  color: #ffffff;
+  font-weight: bold;
+  background: rgba(0, 0, 0, 0.65) !important;
+  border: 2px solid #666666 !important;
+  transition: none !important;
+}
+.apexcharts-tooltip-marker {
+  width: 0.6rem !important;
+  height: 0.6rem !important;
+  margin-right: 0.4rem !important;
+  border-radius: 100% !important;
+}
+.apexcharts-tooltip-series-group.active {
+  padding-bottom: 0 !important;
+}
+.apexcharts-tooltip-text {
+  width: 100%;
+}
+.apexcharts-tooltip-y-group {
+  display: flex;
+  justify-content: space-between;
+}
+.apexcharts-tooltip-text-value {
+  margin-left: 0.5rem !important;
+}
+`;
--- js/index.tsx
+++ js/index.tsx
@@ -3,78 +3,12 @@ import resetCss from "emotion-reset";
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
-
-const scrollbar = {
-  colour: {
-    fg: "rgba(0, 0, 0, 0.2)",
-    bg : "transparent",
-    hover : "rgba(0, 0, 0, 0.3)",
-    active : "rgba(0, 0, 0, 0.5)",
-  },
-  size : "0.5rem",
-};
+import * as globalCss from "./global.styles";
 
 injectGlobal`${resetCss}
-html, body {
-  height: 100%;
-}
-
-#root {
-  height: 100%;
-  overflow: hidden;
-}
-
-
-* {
-  scrollbar-color: ${scrollbar.colour.fg} ${scrollbar.colour.bg};
-  scrollbar-width: thin;
-}
-*::-webkit-scrollbar {
-  height: ${scrollbar.size};
-  width: ${scrollbar.size};
-}
-*::-webkit-scrollbar-track {
-  background-color: ${scrollbar.colour.bg};
-}
-*::-webkit-scrollbar-thumb {
-  background-color: ${scrollbar.colour.fg};
-  border-radius: 4px;
-}
-*::-webkit-scrollbar-thumb:hover {
-  background-color: ${scrollbar.colour.hover};
-}
-*::-webkit-scrollbar-thumb:active {
-  background-color: ${scrollbar.colour.active};
-}
-
-.apexcharts-tooltip {
-  line-height: 0.5;
-  padding: 0.3rem;
-  color: #ffffff;
-  font-weight: bold;
-  background: rgba(0, 0, 0, 0.65) !important;
-  border: 2px solid #666666 !important;
-  transition: none !important;
-}
-.apexcharts-tooltip-marker {
-  width: 0.6rem !important;
-  height: 0.6rem !important;
-  margin-right: 0.4rem !important;
-  border-radius: 100% !important;
-}
-.apexcharts-tooltip-series-group.active {
-  padding-bottom: 0 !important;
-}
-.apexcharts-tooltip-text {
-  width: 100%;
-}
-.apexcharts-tooltip-y-group {
-  display: flex;
-  justify-content: space-between;
-}
-.apexcharts-tooltip-text-value {
-  margin-left: 0.5rem !important;
-}
-`
+${globalCss.fixHeight}
+${globalCss.scrollbar}
+${globalCss.apexcharts}
+`;
 
 ReactDOM.render(<App />, document.getElementById("root"));
