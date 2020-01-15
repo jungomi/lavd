0d882cb318f5c2145cd8e45022e9cd3b1ff00cc6
Michael Jungo <michaeljungo92@gmail.com>
2020-01-15 01:14:50 +0100

    Make the navigation small screen friendly

--- js/Header.styles.ts
+++ js/Header.styles.ts
@@ -3,22 +3,35 @@ import { css, cx } from "emotion";
 export const headerColour = "#353535";
 
 export const header = css({
+  display: "flex",
   position: "relative",
+  justifyContent: "space-between",
   width: "100%",
-  height: `48px`,
-  top: 0,
-  zIndex: 100,
-  background: headerColour
+  height: "3rem",
+  overflow: "auto",
+  background: headerColour,
+  transition: "all 0.2s ease-out",
+  boxSizing: "border-box",
+  padding: "0 1rem"
 });
+
+export const headerOpen = cx(
+  header,
+  css({
+    "@media only screen and (max-width: 896px)": {
+      height: "100%"
+    }
+  })
+);
 
 export const nav = css({
   display: "flex",
-  listStyle: "none",
-  alignItems: "center",
-  margin: "0 auto",
-  padding: "0 2em",
+  flexDirection: "column",
+  flexWrap: "wrap",
+  alignContent: "flex-start",
+  width: "100%",
   overflow: "hidden",
-  height: "100%"
+  transition: "all 0.2s ease-out"
 });
 
 export const item = css({
@@ -26,34 +39,123 @@ export const item = css({
   position: "relative",
   flexDirection: "column",
   justifyContent: "center",
+  flexShrink: 0,
   margin: "0 0.5em",
   textDecoration: "none",
-  height: "100%",
+  height: "3rem",
   color: "#aaa",
   fontWeight: 500,
   userSelect: "none",
   cursor: "pointer",
+  overflow: "hidden",
   // Firefox draws a border on it when clicking, get rid of it.
   outline: "none",
-  ":after": {
-    content: '""',
-    display: "block",
-    background: "#fff",
-    width: "100%",
-    height: "2px",
-    position: "absolute",
-    bottom: 0,
-    transition: "transform .3s ease-out",
-    transform: "translateY(3px)"
+  transition: "padding, height 0.2s ease-out, visibility 0s 0.2s ease-out",
+  "@media only screen and (max-width: 896px)": {
+    // Unfortunately, this works either nicely when the menu is closed, where
+    // the text gets smaller and then half way through it disappears, given the
+    // collapse effect, or when going from small screen to big screen, for which
+    // the visibility needs to turned off immediately, otherwise you can see the
+    // text being pushed off screen.
+    // Since it's more common to stay in a screen size, that has been
+    // prioritised.
+    transition: "padding, height 0.2s ease-out, visibility 0.1s ease-out",
+    height: 0,
+    margin: 0,
+    padding: 0,
+    visibility: "hidden"
   }
 });
+
+export const itemOpen = cx(
+  item,
+  css({
+    "@media only screen and (max-width: 896px)": {
+      width: "inherit",
+      height: "3rem",
+      margin: "inherit",
+      padding: "inherit",
+      visibility: "visible"
+    }
+  })
+);
+
+export const activeOpen = cx(
+  itemOpen,
+  css({
+    color: "#fff"
+  })
+);
 
 export const active = cx(
   item,
   css({
     color: "#fff",
-    ":after": {
-      transform: "translateY(0)"
+    "@media only screen and (max-width: 896px)": {
+      width: "inherit",
+      height: "3rem",
+      margin: "inherit",
+      padding: "unset",
+      visibility: "unset"
+    }
+  })
+);
+
+export const burgerMenu = css({
+  position: "relative",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "3rem",
+  height: "3rem",
+  cursor: "pointer",
+  visibility: "hidden",
+  "@media only screen and (max-width: 896px)": {
+    visibility: "visible"
+  }
+});
+
+export const burgerIcon = css({
+  display: "block",
+  position: "relative",
+  background: "#fff",
+  width: "18px",
+  height: "2px",
+  flexShrink: 0,
+  transition: "all 0.2s ease-out",
+  "::before": {
+    content: "''",
+    position: "absolute",
+    background: "#fff",
+    width: "100%",
+    height: "100%",
+    flexShrink: 0,
+    top: "-5px",
+    transition: "all 0.2s ease-out"
+  },
+  "::after": {
+    content: "''",
+    position: "absolute",
+    background: "#fff",
+    width: "100%",
+    height: "100%",
+    flexShrink: 0,
+    top: "5px",
+    transition: "all 0.2s ease-out"
+  }
+});
+
+export const burgerIconOpen = cx(
+  burgerIcon,
+  css({
+    background: "transparent",
+    "::before": {
+      top: 0,
+      transform: "rotate(45deg)"
+    },
+    "::after": {
+      top: 0,
+      transform: "rotate(-45deg)"
     }
   })
 );
--- js/Header.tsx
+++ js/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as styles from "./Header.styles";
 import { A, usePath } from "hookrouter";
 
@@ -21,58 +21,88 @@ const Link: React.FC<LinkProps> = ({
   return <A {...restProps} href={href} className={actualClassName} />;
 };
 
-export const Header = () => (
-  <header className={styles.header}>
-    <nav className={styles.nav}>
-      <Link
-        href="/scalars"
-        className={styles.item}
-        activeClassName={styles.active}
-      >
-        Scalars
-      </Link>
-      <Link
-        href="/images"
-        className={styles.item}
-        activeClassName={styles.active}
-      >
-        Images
-      </Link>
-      <Link
-        href="/text"
-        className={styles.item}
-        activeClassName={styles.active}
-      >
-        Text
-      </Link>
-      <Link
-        href="/logs"
-        className={styles.item}
-        activeClassName={styles.active}
-      >
-        Logs
-      </Link>
-      <Link
-        href="/markdown"
-        className={styles.item}
-        activeClassName={styles.active}
-      >
-        Markdown
-      </Link>
-      <Link
-        href="/commands"
-        className={styles.item}
-        activeClassName={styles.active}
-      >
-        Commands
-      </Link>
-      <Link
-        href="/about"
-        className={styles.item}
-        activeClassName={styles.active}
-      >
-        About
-      </Link>
-    </nav>
-  </header>
-);
+export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const mediaQuery = window.matchMedia("(min-width: 896px)");
+  // The menu is closed once the media query triggers, whether it goes from
+  // small to big or the other way around doesn't matter.
+  useEffect(() => {
+    const resetOpen = () => setIsOpen(false);
+    // Update when the media media query is toggled.
+    mediaQuery.addListener(resetOpen);
+    return () => {
+      mediaQuery.removeListener(resetOpen);
+    };
+  });
+  const itemClass = isOpen ? styles.itemOpen : styles.item;
+  const activeClass = isOpen ? styles.activeOpen : styles.active;
+  const iconClass = isOpen ? styles.burgerIconOpen : styles.burgerIcon;
+  const clickLink = () => {
+    setIsOpen(false);
+  };
+  return (
+    <header className={isOpen ? styles.headerOpen : styles.header}>
+      <nav className={styles.nav}>
+        <Link
+          href="/scalars"
+          onClick={clickLink}
+          className={itemClass}
+          activeClassName={activeClass}
+        >
+          Scalars
+        </Link>
+        <Link
+          href="/images"
+          onClick={clickLink}
+          className={itemClass}
+          activeClassName={activeClass}
+        >
+          Images
+        </Link>
+        <Link
+          href="/text"
+          onClick={clickLink}
+          className={itemClass}
+          activeClassName={activeClass}
+        >
+          Text
+        </Link>
+        <Link
+          href="/logs"
+          onClick={clickLink}
+          className={itemClass}
+          activeClassName={activeClass}
+        >
+          Logs
+        </Link>
+        <Link
+          href="/markdown"
+          onClick={clickLink}
+          className={itemClass}
+          activeClassName={activeClass}
+        >
+          Markdown
+        </Link>
+        <Link
+          href="/commands"
+          onClick={clickLink}
+          className={itemClass}
+          activeClassName={activeClass}
+        >
+          Commands
+        </Link>
+        <Link
+          href="/about"
+          onClick={clickLink}
+          className={itemClass}
+          activeClassName={activeClass}
+        >
+          About
+        </Link>
+      </nav>
+      <div onClick={() => setIsOpen(!isOpen)} className={styles.burgerMenu}>
+        <div className={iconClass} />
+      </div>
+    </header>
+  );
+};
