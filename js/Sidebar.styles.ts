9d799824e61557b8ee7262ae3e964b9e901d3348
Michael Jungo <michaeljungo92@gmail.com>
2020-07-27 21:55:29 +0200

    Fix spacing between filter text and clear symbol

--- js/Sidebar.styles.ts
+++ js/Sidebar.styles.ts
@@ -182,7 +182,10 @@ export const visibilityIconHidden = cx(
 );
 
 // Based on command styles
-export const input = cx(commandStyles.input, css({ margin: "0 0.4rem" }));
+export const input = cx(
+  commandStyles.input,
+  css({ margin: "0 0.4rem", paddingRight: "1.3rem" })
+);
 export const inputContainer = cx(
   commandStyles.inputContainer,
   css({ marginTop: 0, marginBottom: "1rem" })
